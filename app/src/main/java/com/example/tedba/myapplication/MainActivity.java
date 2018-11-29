eed91ec7bf9152819d61f4f88541fd89ad438bbe
tedshaoxue <shaoxue2@illinois.edu>
2018-11-28 19:48:24 -0600

    added findAntonyms function

--- app/src/main/java/com/example/tedba/myapplication/MainActivity.java
+++ app/src/main/java/com/example/tedba/myapplication/MainActivity.java
@@ -5,11 +5,30 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.JsonReader;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import org.json.*;
 
 public class MainActivity extends AppCompatActivity {
+    /**
+     * url to call https://www.dictionaryapi.com/api/v3/references/thesaurus/json/umpire?key=your-api-key
+     */
+
+    public static String findAntnonyms(final String word) {
+        String antonyms = "";
+        JsonReader parser = new JsonParser();
+        JSONObject result = parser.parse(word).getAsJsonObject();
+        if (result.has("noun")) {
+            JSONObject type = result.getJSONObject("noun");
+            JSONArray ant = type.getJSONArray("ant");
+            for (int i = 0; i < ant.length() - 1; i++) {
+                antonyms += ant.get(i).toString() + ", ";
+            }
+
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
