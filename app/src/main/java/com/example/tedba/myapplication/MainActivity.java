38f074ab9c1606baeb93d62799a0e47c43e89769
tedshaoxue <shaoxue2@illinois.edu>
2018-12-01 11:54:33 -0600

    added api call

--- app/src/main/java/com/example/tedba/myapplication/MainActivity.java
+++ app/src/main/java/com/example/tedba/myapplication/MainActivity.java
@@ -5,7 +5,6 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.JsonReader;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
@@ -13,7 +12,13 @@ import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 
-import org.json.*;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -22,22 +27,12 @@ public class MainActivity extends AppCompatActivity {
      */
     /* Default Logging Tag */
     private static final String TAG = "Antonym Finder";
+    private static RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         /*
         Button handlers for antonym search
         */
@@ -57,7 +52,33 @@ public class MainActivity extends AppCompatActivity {
     public void findAntonyms() {
         final EditText enter_word = (EditText) findViewById(R.id.enter_word);
         final EditText displayAntonym = (EditText) findViewById(R.id.displayAntonym);
+        String wordSearched = enter_word.getText().toString();
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
+                "https://www.dictionaryapi.com/api/v3/references/thesaurus/json/" + wordSearched +
+                        "?key=e267e542-c713-405e-9de9-916e1f134128",
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(final JSONObject response) {
+                        Log.d(TAG, response.toString());
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(final VolleyError error) {
+                Log.w(TAG, error.toString());
+            }
+        });
     }
+
+
+
+
+
+
+
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
